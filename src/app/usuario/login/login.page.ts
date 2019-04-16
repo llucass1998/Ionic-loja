7506838f08611edf6956ca9b2c64419d6aafc9c1
m4rcos-mk <rezendemarcos4@gmail.com>
2019-04-15 21:41:55 -0300

    150419.2

--- src/app/usuario/login/login.page.ts
+++ src/app/usuario/login/login.page.ts
@@ -1,34 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth'
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AlertController } from "@ionic/angular";
+import { auth } from "firebase";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
-  
-  private email:string;
-  private pws:string;
-
+  private uid: string;
+  private email: string;
+  private pws: string;
 
   constructor(
-    private afAuth:AngularFireAuth
-  ) { }
+    private afAuth: AngularFireAuth,
+    public alertController: AlertController
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit(form){
+  onSubmit(form) {
     this.login();
   }
 
-  login(){
-    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.pws)
-    .then(
-      user=>{
-        console.log(user);
-    })
+  login() {
+    this.afAuth.auth
+      .signInWithEmailAndPassword(this.email, this.pws)
+      .then(
+        res => {
+          console.log(res);
+          this.uid = res.user.uid;
+        },
+        erro => {
+          console.log(erro);
+          this.presentAlert("Erro!", "Usuário não encontrado!");
+          
+        }
+      )
+      .catch(erros => {
+        this.presentAlert("Erro no Sistema!", "Não foi possível conectar!");
+      });
   }
 
+  logout() {
+    this.afAuth.auth.signOut();
+    this.uid = null;
+  }
+
+  addUser() {
+    this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.pws).then(
+      ok => {
+        this.presentAlert("OK!", "Usuário cadastrado!");
+      },
+      err => {
+        this.presentAlert(
+          "Opss!",
+          "E-mail ou senhas inválidas para autenticação! Tente novamente."
+        );
+      }
+    );
+  }
+
+  loginG() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(
+      res => {
+        console.log(res);
+        this.uid = res.user.uid;
+      }
+    );
+  }
+
+  async presentAlert(tipo: string, texto: string) {
+    const alert = await this.alertController.create({
+      header: tipo,
+      //subHeader: "Subtitle",
+      message: texto,
+      buttons: ["OK"]
+    });
+
+    await alert.present();
+  }
 }
--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,2 +1,8 @@
 export class Usuario {
+    uid: string;
+    nome: string;
+    email: string;
+    pws: string;
+    foto: string;
+    ativo: boolean = true;
 }
